5fcc9227dbeaa6db32467ac8ca1fd3ff13633603
Rikuhiro G. Yamada <rikuhiro@gmail.com>
2020-04-30 12:15:52 +0900

    implementing video utils

--- convert_video.py
+++ convert_video.py
@@ -1,19 +1,164 @@
 import argparse
+import subprocess
+from datetime import datetime
 import os
 from glob import glob
+import re
+import sys
+
+
+def interpret_datetimestr(datetime_str):
+    """ Find a datetime string and convert it to a datatime object 
+    allowing some variant forms
+    
+    Args:
+        datetime_str (string): a string containing datetime
+    
+    Returns:
+        a datetime object
+    
+    Raises:
+        ValueError: raised when interpretation is failed
+    """
+
+    datestr_patterns = [r'(\d{4})(\d{2})(\d{2})',
+                        r'(\d{4})/(\d{1,2})/(\d{1,2})',
+                        r'(\d{4})-(\d{1,2})-(\d{1,2})']
+
+    timestr_patterns = [r'(\d{2})(\d{2})(\d{2})',
+                        r'(\d{1,2}):(\d{1,2}):(\d{1,2})',
+                        r'(\d{1,2})-(\d{1,2})-(\d{1,2})']
+
+    datetime_obj = None
+    for pat in datestr_patterns:
+        matched = re.search(pat, datetime_str)
+        if matched:
+            year = int(matched.group(1))
+            month = int(matched.group(2))
+            day = int(matched.group(3))
+            datetime_str = re.sub(pat, '', datetime_str)
+
+            for pat_time in timestr_patterns:
+                matched_time = re.search(pat_time, datetime_str)
+                if matched_time:
+                    hour = int(matched_time.group(1))
+                    minuite = int(matched_time.group(2))
+                    second = int(matched_time.group(3))
+                    datetime_obj = datetime(year, month, day,
+                                            hour, minuite, second)
+                    break
+            if not matched_time:
+                datetime_obj = datetime(year, month, day)
+    if not datetime_obj:
+        raise ValueError(
+            'failed to interpret datetime string \'{}\''.format(datetime_str))
+
+    return(datetime_obj)
+
+
+def call_proc(input_filepath, output_dir, camera_id, video_start_dt_str, encoder):
+    """ invokes an ffmpeg subprocess
+
+    Arguments:
+        input_filepath {str} -- a path to the video file to be converted
+        output_dir {str} -- a path to the output directry
+        camera_id {str} -- a label to be added in the resulting video file "[camera_id]_[video_start_dt].mp4"
+        video_start_dt_str {str} -- a datetime string of the video start
+        encoder {str} -- a string to be passed to ffmpeg's -c:v option for encoder (e.g. "h264" or "h264_nvenc")
+
+    Returns:
+        dict -- "proc": subprocess object, "filestem": output filename without the extention 
+    """
+    output_filestem = f'{camera_id}_{video_start_dt_str}'
+    output_filepath = os.path.join(
+        output_dir, output_filestem + '.mp4')
+    cmd = ['ffmpeg', '-i', input_filepath, '-c:v', encoder, '-an',
+            '-b:v', '0', '-cq', '32', output_filepath, '-y']
+    print(' '.join(cmd))
+
+    # open a log file
+    os.makedirs(os.path.join(output_dir, 'logs'), exist_ok=True)
+    with open(os.path.join(output_dir, 'logs', output_filestem + '.log'), 'w') as logout:
+        # start a subprocess
+        proc = subprocess.Popen(cmd, stdout=logout, stderr=logout, text=True)
+    
+    return {'proc':proc, 'filestem':output_filestem}
+
+
+def get_start_dt(video_filepath):
+    """ obtain the timestamp of the video start
+
+    Arguments:
+        video_filepath {str} -- the path to the target video file
+
+    Returns:
+        datetime --- a datetime object of the start time
+    """
+
+    try:
+        filename = os.path.basename(video_filepath)
+        start_datetime = interpret_datetimestr(filename)
+    except ValueError:
+        print(f'[warning] Failed to interpret the start datetime from the filename "{filename}"')
+        start_datetime = None
+
+    return start_datetime
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target_dir", required=True, help="path to the target directory")
     parser.add_argument("-o", "--output_dir", required=True, help="path to the directory of the resulting video clips")
-    
+    parser.add_argument("-w", "--worker", help="the number of parallel workers", type=int, default=1)
+    parser.add_argument("-e", "--encoder", help="a string to be passed to ffmpeg -c:v option for the encoder", default="h264")
+
     args = parser.parse_args()
 
     target_dir = args.target_dir
     output_dir = args.output_dir
+    worker_num = args.worker
+    encoder = args.encoder
 
-    file_list = glob(os.path.join(target_dir, '**/*.avi'))
-    
+    # recursively get contents of the target dir
+    # assuming the structure of [camera_ids]/[video files]
+    file_list = [f for f in glob(os.path.join(target_dir, '**/*')) if re.search(r'.*\.(avi|mp4)', f)]
 
-    print('list')
-    print('\n'.join(file_list))
+#    print('\n'.join(file_list))   
+    os.makedirs(target_dir, exist_ok=True)
+
+    video_count = 0
+    more_video = True
+    processes =[]
+    while more_video:
+        i_video_path = file_list[video_count]
+
+        # get the parent dir name as a camera id
+        camera_dir_path, video_filename = os.path.split(i_video_path)
+        camera_id = os.path.split(camera_dir_path)[1]
+
+        start_dt = get_start_dt(i_video_path)
+        if start_dt:
+            start_dt_str = start_dt.strftime("%Y-%m-%d_%H-%M-%S")
+        else:
+            # use original filename when failed to get start_dt
+            start_dt_str = os.path.splitext(video_filename)[0]
+        
+        for i in range(worker_num):
+            output_subdir = os.path.join(output_dir, camera_id)
+            os.makedirs(output_subdir, exist_ok=True)
+            p = call_proc(i_video_path, output_subdir, camera_id, start_dt_str, encoder)
+            processes.append(p)
+            if (len(file_list) - 1) > video_count:
+                video_count += 1
+            else:
+                more_video = False
+                break
+        
+        for p in processes:
+            p['proc'].wait()
+            if p['proc'].returncode != 0:
+                print(f'ffmpeg was exited with error. See log files for detail: {p["filestem"]}.log')
+
+
+        # clear workers
+        process = []
