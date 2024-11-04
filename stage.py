b449275973aba579f7262a5191603c49ab3ce50e
ygriku <rikuhiro@gmail.com>
2024-11-04 21:23:25 +0900

    [UPD] updated the version number

--- stage.py
+++ stage.py
@@ -24,7 +24,7 @@ from logging import getLogger, StreamHandler, FileHandler, Formatter
 import traceback
 
 
-FASTER2_NAME = 'FASTER2 version 0.5.1'
+FASTER2_NAME = 'FASTER2 version 0.5.2'
 STAGE_LABELS = ['Wake', 'REM', 'NREM']
 XLABEL = 'Total low-freq. log-powers'
 YLABEL = 'Total high-freq. log-powers'
